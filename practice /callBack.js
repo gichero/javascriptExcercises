24318affb1774e7a512049991e8e53d5f3c6c904
Amos <amosgathungu@gmail.com>
2017-03-20 22:20:30 -0400

    setTimeout added

--- practice /callBack.js
+++ practice /callBack.js
@@ -1,65 +1,61 @@
 //Nested Callbacks 1
-function square(num, callBack){
-    var result = num * num;
-    callBack(result);
-}
-
-var callMeback = function callMeBack(result) {
-  console.log('result is: ', result);
-  };
-  square(5, callMeback);
-
-//Nested Callbacks 2
-function square(num, callBack){
-    var result = num * num;
-    callBack(result);
-}
-
-function add(x, y, callback){
-    var result = x + y;
-    callback(result);
-}
-
- var x = 4;
- var y = 3;
-
-square(x, function(x2){
-    square(y, function(y2){
-        add(x2, y2, function(result){
-            console.log(result);
-        });
-    });
-});
+// function square(num, callBack){
+//     var result = num * num;
+//     callBack(result);
+// }
+//
+// var callMeback = function callMeBack(result) {
+//   console.log('result is: ', result);
+//   };
+//   square(5, callMeback);
+//
+// //Nested Callbacks 2
+// function square(num, callBack){
+//     var result = num * num;
+//     callBack(result);
+// }
+//
+// function add(x, y, callback){
+//     var result = x + y;
+//     callback(result);
+// }
+//
+//  var x = 4;
+//  var y = 3;
+//
+// square(x, function(x2){
+//     square(y, function(y2){
+//         add(x2, y2, function(result){
+//             console.log(result);
+//         });
+//     });
+// });
 
 //Nested callbacks 3
+var x = 4;
+var y = 3;
 
 function square(num, callBack){
-    var result = num * num;
-    callBack(result);
+    setTimeout(function(){
+    var sqNum = num * num;
+    callBack(sqNum);
+}, 1000);
 }
 
 function squareRoot(num, callBack){
-    var root = Math.sqrt(num);
-    callBack(root);
+    setTimeout(function(){
+    var sqRoot = Math.sqrt(num);
+    callBack(sqRoot);
+}, 1000);
 }
 
-function add(x, y, callback){
-    var result = x + y;
-    callback(result);
-}
-
-var x = 16;
-var y = 9;
-
-square(x, function(x2){
-   square(y, function(y2){
-       squareRoot(x, function(x){
-           squareRoot(y,function(y){
-               console.log(root);
-       add(x2, y2, function(result){
-           console.log(result);
-           });
-           });
+square(x, function(xSquare){
+    console.log(xSquare);
+   square(y, function(ySquare){
+       console.log(ySquare);
+       var sum = xSquare + ySquare;
+       squareRoot(sum, function(sqRoot){
+           console.log(sqRoot);
        });
    });
 });
